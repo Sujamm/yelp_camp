f350a67aa719a7042f848587ea52a18ecacbb69c
Sujaira Moughawiche <susi141@gmail.com>
2019-11-23 01:59:53 +0000

    Styling campgrounds show page

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost/yelp_camp", { useUnifiedTopology: true,
 useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 //---- ROUTES ------
 //HOME - Landing page
--- seeds.js
+++ seeds.js
@@ -6,17 +6,17 @@ var data = [
 	{
 		name:"Clound's Rest",
 		image: "https://cdn.pixabay.com/photo/2018/12/24/22/21/camping-3893598_960_720.jpg",
-		description: "A nice place to go camping with the family"
+		description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 	}, 	
 	{
 		name:"Deseert Mesa",
 		image: "https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022_960_720.jpg",
-		description: "BLABLABLABLA"
+		description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 	}, 	
 	{
 		name:"Canyon Floor",
 		image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402_960_720.jpg",
-		description: "BLABLABLABLA"
+		description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
 	}
 ];
 
